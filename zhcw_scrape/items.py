5c281319e78079d1ef4460e18a1da8464877678c
HIMGITHUB <him666@HIM666deMacBook-Pro.local>
2016-08-30 19:00:32 +0800

    just for check

--- zhcw_scrape/items.py
+++ zhcw_scrape/items.py
@@ -10,6 +10,8 @@ import scrapy
 
 class ZhcwScrapeItem_brief(scrapy.Item):
     # define the fields for your item here like:
+    """#获奖情况摘要
+    """
     """#开奖日期
     """
     open_date = scrapy.Field()
@@ -84,7 +86,37 @@ class ZhcwScrapeItem_brief(scrapy.Item):
 class ZhcwScrapeItem_infos(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-
-    #
+    """#各省开奖情况详情
+    """
+    """#省名
+    """
+    state_name = scrapy.Field()
+    """#开奖期号
+    """
+    issue_no = scrapy.Field()
+    """#省内销售额
+    """
+    state_ticket_sold = scrapy.Field()
+    """#1等奖省内获奖人数
+    """
+    state_wwp1 = scrapy.Field()
+    """#2等奖省内获奖人数
+    """
+    state_wwp2 = scrapy.Field()
+    """#3等奖省内获奖人数
+    """
+    state_wwp3 = scrapy.Field()
+    """#4等奖省内获奖人数
+    """
+    state_wwp4 = scrapy.Field()
+    """#5等奖省内获奖人数
+    """
+    state_wwp5 = scrapy.Field()
+    """#6等奖省内获奖人数
+    """
+    state_wwp6 = scrapy.Field()
+    """#幸运二等奖等奖省内获奖人数
+    """
+    state_luckysecond = scrapy.Field()
     pass
 
