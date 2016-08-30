8fb625f265e0a9fe0fb32d750af9a3035ef2b118
HIMGITHUB <him666@HIM666deMacBook-Pro.local>
2016-08-30 18:27:56 +0800

    Initial commit build in 20160830

---
+++ main.py
@@ -0,0 +1,4 @@
+# coding = UTF-8c
+__author__ = 'Xdtherion'
+from scrapy import cmdline
+cmdline.execute("scrapy crawl zhcw_spider".split())
---
+++ zhcw_scrape/items.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class ZhcwScrapeItem_brief(scrapy.Item):
+    # define the fields for your item here like:
+    """#开奖日期
+    """
+    open_date = scrapy.Field()
+    """#开奖期号
+    """
+    issue_no = scrapy.Field()
+    """#销售总额
+    """
+    ticket_sold = scrapy.Field()
+    """#奖池累计总额
+    """
+    money_still = scrapy.Field()
+    """#蓝色球号码
+    """
+    blue_no = scrapy.Field()
+    """#红球1
+    """
+    red1 = scrapy.Field()
+    """#红球2
+    """
+    red2 = scrapy.Field()
+    """#红球3
+    """
+    red3 = scrapy.Field()
+    """#红球4
+    """
+    red4 = scrapy.Field()
+    """#红球5
+    """
+    red5 = scrapy.Field()
+    """#红球6
+    """
+    red6 = scrapy.Field()
+    """#1等奖金额
+    """
+    prize1 = scrapy.Field()
+    """#2等奖金额
+    """
+    prize2 = scrapy.Field()
+    """#3等奖金额
+    """
+    prize3 = scrapy.Field()
+    """#4等奖金额
+    """
+    prize4 = scrapy.Field()
+    """#5等奖金额
+    """
+    prize5 = scrapy.Field()
+    """#6等奖金额
+    """
+    prize6 = scrapy.Field()
+    """#1等奖获奖人数
+    """
+    wwp1 = scrapy.Field()
+    """#2等奖获奖人数
+    """
+    wwp2 = scrapy.Field()
+    """#3等奖获奖人数
+    """
+    wwp3 = scrapy.Field()
+    """#4等奖获奖人数
+    """
+    wwp4 = scrapy.Field()
+    """#5等奖获奖人数
+    """
+    wwp5 = scrapy.Field()
+    """#6等奖获奖人数
+    """
+    wwp6 = scrapy.Field()
+
+    pass
+class ZhcwScrapeItem_infos(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+
+    #
+    pass
+
